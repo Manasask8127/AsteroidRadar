353b4d1e0b3d1b744e0491ed9da8248ea9f3ba7b
Manasask8127 <manasask8127@gmail.com>
2022-04-18 13:57:05 +0530

    error resolved

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,10 +1,12 @@
 package com.udacity.asteroidradar
 
+import com.udacity.asteroidradar.NeoWsApiKey
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val NeoW_key="yNAz2FFEetM0U65ckGlux0zyaUrJWA3LH60aTIPg"
+    const val NeoW_key=NeoWsApiKey.api_key
     const val IMAGE_TYPE="image"
     const val DELETE_ASTEROIDS_NAME="DeleteAsteroidName"
 }
